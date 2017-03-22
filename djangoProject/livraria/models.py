6a8c9c957d797c86c0a287fe1656068d3fd76697
michel spirlandeli <michelspirlandeli10@gmail.com>
2017-03-22 14:17:23 -0300

    Second Commit

--- djangoProject/livraria/models.py
+++ djangoProject/livraria/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class livro (models.Model):
+class Livro (models.Model):
     autor = models.CharField(max_length=50)
     editora = models.CharField(max_length=50)
     isbn = models.IntegerField()
--- djangoProject/livraria/views.py
+++ djangoProject/livraria/views.py
@@ -1,3 +1,15 @@
+from django.forms import ModelForm
+from .models import *
 from django.shortcuts import render
 
 # Create your views here.
+
+class LivroForm(ModelForm):
+    class Meta:
+        model = Livro
+        fields = ['autor','editora', 'isbn', 'numeroPaginas', 'titulo', 'anoPublicacao', 'emailEditora']
+
+def livro_list(request, template_name='livro_list.html'):
+    livro = Livro.objects.all()
+    livros = {'lista': livro}
+    return render(request, template_name, livros)
